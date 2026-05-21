e1e2ac68846d0e8a3e87453d76451a0151f24e41
Ganesh Lakade <Ganesh.Lakade@INFOVISIONLABS.COM>
2026-05-21 20:12:45 +0530

    updated code

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Ganesh + Shubham + Avadhut= Angar Aag DHurrrr";
+        return "Ganesh + Shubham + Avadhut= Angar Aag";
     }
 }
